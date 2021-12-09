d7d8a8428e1c18c5d8ebb2da6efa3500414cd0f7
Artur <artur-avetisyan-90@inbox.ru>
2021-12-09 16:45:20 +0300

    class commit

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-print("welcome")
+# Паттерны проектирования
+from abc import abstractmethod, ABC
+
+class Logistics(ABC):
+    @abstractmethod
+    def create_transport(self):
+        pass
+
+# class
+
 
 
