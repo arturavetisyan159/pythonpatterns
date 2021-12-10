fa16f8a433ab73ecbc4238d9a79f48ffd5826f69
Artur <artur-avetisyan-90@inbox.ru>
2021-12-10 12:33:15 +0300

    class abstract fabric start

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Паттерны проектирования
+# Фабричный метод, Factory method, виртуальный конструктор
+
 from abc import abstractmethod, ABC
 
 class Logistics(ABC):
@@ -6,7 +8,54 @@ class Logistics(ABC):
     def create_transport(self):
         pass
 
-# class
+    def connect_to_db(self):
+        pass
+
+    def get_data_from_db(self):
+        pass
+
+
+class GroundLogistics(Logistics):
+    def create_transport(self):
+        return Truck()
+
+
+class SeaLogistics(Logistics):
+    def create_transport(self):
+        return Boat()
+
+
+class ITransport():
+    def deliver(self):
+        pass
+
+
+class Truck(ITransport):
+    def deliver(self):
+        print("Грузовик едет по суше")
+
+
+class Boat(ITransport):
+    def deliver(self):
+        print("Лодка идет по воде")
+
+# Абстрактная фабрика, Abstract Fabric
+
+
+# def main():
+#     ground_logistic = GroundLogistics()
+#     sea_logistic = SeaLogistics()
+#
+#     truck = ground_logistic.create_transport()
+#     truck.deliver()
+#
+#     boat = sea_logistic.create_transport()
+#     boat.deliver()
+#
+# if __name__ == '__main__':
+#     main()
+
+
 
 
 
