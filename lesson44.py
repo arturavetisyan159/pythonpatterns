27cf769939bd85aa1012d450ec6d57bec50faf5d
Artur <artur-avetisyan-90@inbox.ru>
2021-12-16 17:29:10 +0300

    class - solid passed

--- lesson44.py
+++ lesson44.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 # Паттерн "Стратегия"
 
 # Dyson V11
@@ -81,51 +82,137 @@ class PalaceStrategy(ICleanStrategy):
 # сущность открыта для расширения, закрыта для модификации.
 # расширение имеется в виду наследование.
 
-class Animal:
-    def __init__(self, name):
-        self.name = name
+# class Animal:
+#     def __init__(self, name):
+#         self.name = name
+#
+#     def get_animal_name(self):
+#         return self.name
+#
+#     def make_sound(self):
+#         pass
+#
+# class Cat(Animal):
+#     def __init__(self, name="cat"):
+#         super().__init__(name)
+#
+#     def make_sound(self):
+#         print("Meow")
+#
+#
+# class Dog(Animal):
+#     def __init__(self, name="dog"):
+#         super().__init__(name)
+#
+#     def make_sound(self):
+#         print("Bark")
+#
+#
+# class Cow(Animal):
+#     def __init__(self, name="cow"):
+#         super().__init__(name)
+#
+#     def make_sound(self):
+#         print("mooh")
+#
+# animals = [Cat(), Dog(), Cow()]
+#
+# def get_animal_name(animals: list[Animal]):
+#     for an in animals:
+#         an.make_sound()
 
-    def get_animal_name(self):
-        return self.name
 
-    def make_sound(self):
+# L - Liskov Substitution Principle (Принцип подстановки Лискова)
+# необходимо, чтобы потомки служили заменой своим родителям.
+
+# class Animal:
+#     def __init__(self, name, legs):
+#         self.name = name
+#         self.legs = legs
+#
+#     def get_animal_legs(self): # один общий метод на все.
+#         return self.legs
+#
+# class Cat(Animal):
+#     def __init__(self, name="cat", legs=4):
+#         super().__init__(name, legs)
+#
+#     def make_sound(self):
+#         print("Meow")
+#
+#
+# class Dog(Animal):
+#     def __init__(self, name="dog", legs=4):
+#         super().__init__(name, legs)
+#
+#     def make_sound(self):
+#         print("Bark")
+#
+# animals = [Cat(), Dog()]
+
+
+##########################
+# I - Interface Sigregation (Принцип раздедления интерфейса)
+# Создавайте узкоспециализированные интерфейсы, предназначенные для конкретного клиента. Клиенты не должн зависеть
+# от интерфейсов, котрые они не используют
+
+
+# class IFigure(ABC):
+#     @abstractmethod
+#     def draw(self):
+#        pass
+#
+#
+# class Circle(IFigure):
+#     def draw(self):
+#         print("draw circle")
+#
+#
+# class Square(IFigure):
+#     def draw(self):
+#         print("draw square")
+#
+#
+# class Rectangle(IFigure):
+#     def draw(self):
+#         print("draw rectangle")
+#
+#
+# figures = [Circle(), Rectangle(), Square()]
+
+###################
+# D - Dependency Inversion Principle (Принцип Инверсии Зависимостей)
+# Объектом зависимости должна быть абстракция, а не что то конкретное.
+# Модули верхних модулей не должны зависеть от модулей нижних уровней. Оба типа модулей должны зависеть от абстракций.
+# Абстракции не должны зависеть от деталей. Это детали должны зависеть от абстракций.
+
+class ConsolePrinter:
+    def print(self):
         pass
 
-class Cat(Animal):
-    def __init__(self, name="cat"):
-        super().__init__(name)
+    def do_smth_with_internal_params1(self):
+        pass
 
-    def make_sound(self):
-        print("Meow")
+    def do_smth_with_internal_params2(self):
+        pass
 
-
-class Dog(Animal):
-    def __init__(self, name="dog"):
-        super().__init__(name)
-
-    def make_sound(self):
-        print("Bark")
+    def public_interface_with_internal_logic(self):
+        a = self.printer.do_smth_with_internal_params1()
+        b = self.printer.do_smth_with_internal_params2()
+        c = a + b
+        return c
 
 
-class Cow(Animal):
-    def __init__(self, name="cow"):
-        super().__init__(name)
+class Book:
+    def __init__(self):
+        self.printer = ConsolePrinter()
 
-    def make_sound(self):
-        print("mooh")
-
-animals = [Cat(), Dog(), Cow()]
-
-def get_animal_name(animals: list[Animal]):
-    for an in animals:
-        an.make_sound()
-
-
-
-
+    def print(self):
+        a = self.printer.public_interface_with_internal_logic()
+        return a
 
 def main():
     pass
 
 if __name__ == "__main__":
-    get_animal_name(animals)
+    main()
