c4dcd72343bb05cdcbf995a4086f704847397a92
Artur <artur-avetisyan-90@inbox.ru>
2022-01-18 22:28:13 +0300

    cc

--- lesson47/process_image.py
+++ lesson47/process_image.py
@@ -1,19 +1,20 @@
 from PIL import Image, ImageFilter
+import multiprocessing
 import time
 
 def filter_image(img_name):
-    print(f"Работа с картинкой {img_name} началась!")
+    print(f"{multiprocessing.current_process().name}: Работа с картинкой {img_name} началась!")
     img = Image.open(f"images/{img_name}")
     img = img.filter(ImageFilter.GaussianBlur(15))
     img.save(f"processed_images/{img_name}")
-    print(f"Работа с картинкой {img_name} завершена!")
+    print(f"{multiprocessing.current_process().name}: Работа с картинкой {img_name} завершена!")
 
 
 def main():
     images = [
-        "516_1.jpeg",
-        "516_2.jpeg",
-        "516_3.png",
+        "516_1.jpg",
+        "516_2.jpg",
+        "516_3.jpg",
         "516_4.jpg",
         "516_5.jpg",
         "516_6.jpg",
@@ -21,10 +22,19 @@ def main():
         "516_8.jpg",
         "516_9.jpg"
     ]
+    processes = []
     start_time = time.time()
 
-    for img in images:
-        filter_image(img)
+    # for img in images:
+    #     filter_image(img)
+
+    for idx, img in enumerate(images, start=1):
+        proc = multiprocessing.Process(target=filter_image, name=f"Процесс{idx}", args=(img,))
+        processes.append(proc)
+        proc.start()
+
+    for proc in processes:
+        proc.join()
 
     end_time = time.time()
     print(f"Время исполнения программы: {(end_time - start_time):.2f} секунд")
