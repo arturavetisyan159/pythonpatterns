d3859364bf02261367d94afdaa32f914dff82302
Artur <artur-avetisyan-90@inbox.ru>
2022-01-18 21:44:11 +0300

    done

--- main.py
+++ main.py
@@ -8,15 +8,19 @@ def read_list(spisok):
         for el in spisok:
             if el % 2 != 0:
                 result.append(el)
-        print(f"Результат работы {multiprocessing.current_process().name} = {result}")
+        with open ("file_1.txt", "w", encoding="utf-8") as f:
+            f.write(f"{result}")
+        print(f"{multiprocessing.current_process().name} отработала")
     else:
         for el in spisok:
             if el % 2 == 0:
                 result.append(el)
-        print(f"Результат работы {multiprocessing.current_process().name} = {result}")
+        with open("file_2.txt", "w", encoding="utf-8") as f:
+            f.write(f"{result}")
+        print(f"{multiprocessing.current_process().name} отработала")
 
 def main():
-    spisok = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    spisok = [i for i in range(1, 100)]
     # proc1 = multiprocessing.Process(target=sleeper, name="Процесс 1", args=(sec,))
     # proc2 = multiprocessing.Process(target=sleeper, name="Процесс 2", args=(sec,))
 
