9611a97c3c46df50169b4a64ff26709bb4f97409
Artur <artur-avetisyan-90@inbox.ru>
2022-01-18 15:18:59 +0300

    class- added images

--- lesson47/process_image.py
+++ lesson47/process_image.py
@@ -2,8 +2,15 @@ from PIL import Image, ImageFilter
 
 def main():
     images = [
-        "tg avatar"
-
+        "516_1.jpeg",
+        "516_2.jpeg",
+        "516_3.jpeg",
+        "516_4.jpeg",
+        "516_5.jpeg",
+        "516_6.jpeg",
+        "516_7.jpeg",
+        "516_8.jpeg",
+        "516_9.jpeg",
     ]
 
 if __name__ == "__main__":
